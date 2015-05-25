7578881e47195b730f706bb4a867434f41639af5
Onni Hakala <onni@seravo.fi>
2015-05-26 00:11:51 +0300

    update rest client

--- lib/diplomat/rest_client.rb
+++ lib/diplomat/rest_client.rb
@@ -4,16 +4,48 @@ require 'json'
 module Diplomat
   class RestClient
 
+    @access_methods = []
+
+    # Initialize the fadaray connection
+    # @param api_connection [Faraday::Connection,nil] supply mock API Connection
     def initialize api_connection=nil
       start_connection api_connection
     end
 
+    # Format url parameters into strings correctly
+    # @param name [String] the name of the parameter
+    # @param value [String] the value of the parameter
+    # @return [Array] the resultant parameter string inside an array.
+    def use_named_parameter(name, value)
+      if value then ["#{name}=#{value}"] else [] end
+    end
+
+    # Assemble a url from an array of parts.
+    # @param parts [Array] the url chunks to be assembled
+    # @return [String] the resultant url string
     def concat_url parts
-        if parts.length > 1 then
-            parts.first << '?' << parts.drop(1).join('&')
-        else
-            parts.first
-        end
+      if parts.length > 1 then
+        parts.first + '?' + parts.drop(1).join('&')
+      else
+        parts.first
+      end
+    end
+
+    class << self
+
+      def access_method? meth_id
+        @access_methods.include? meth_id
+      end
+
+      # Allow certain methods to be accessed
+      # without defining "new".
+      # @param meth_id [Symbol] symbol defining method requested
+      # @param *args Arguments list
+      # @return [Boolean]
+      def method_missing(meth_id, *args)
+        access_method?(meth_id) ? new.send(meth_id, *args) : super
+      end
+
     end
 
     private
@@ -21,13 +53,47 @@ module Diplomat
     # Build the API Client
     # @param api_connection [Faraday::Connection,nil] supply mock API Connection
     def start_connection api_connection=nil
-      @conn = api_connection ||= Faraday.new(:url => Diplomat.configuration.url) do |faraday|
+      @conn = build_connection(api_connection)
+      @conn_no_err = build_connection(api_connection, true)
+    end
+
+    def build_connection(api_connection, raise_error=false)
+      return api_connection || Faraday.new(:url => Diplomat.configuration.url) do |faraday|
+        faraday.adapter  Faraday.default_adapter
         faraday.request  :url_encoded
+        faraday.response :raise_error unless raise_error
+
         Diplomat.configuration.middleware.each do |middleware|
           faraday.use middleware
         end
-        faraday.adapter  Faraday.default_adapter
-        faraday.use      Faraday::Response::RaiseError
+      end
+    end
+
+    # Parse the body, apply it to the raw attribute
+    def parse_body
+      @raw = JSON.parse(@raw.body)
+    end
+
+    # Get the key/value(s) from the raw output
+    def return_value
+      if @raw.count == 1
+        @value = @raw.first["Value"]
+        @value = Base64.decode64(@value) unless @value.nil?
+      else
+        @value = @raw.map do |e|
+          {
+            :key => e["Key"],
+            :value => (Base64.decode64(e["Value"]) unless e["Value"].nil?)
+          }
+        end
+      end
+    end
+
+    # Get the name and payload(s) from the raw output
+    def return_payload
+      @value = @raw.map do |e|
+        { :name => e["Name"],
+          :payload => (Base64.decode64(e["Payload"]) unless e["Payload"].nil?) }
       end
     end
 
